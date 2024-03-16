1c41fb253de5e68ddd43de301eb1909f4076f203
s27051 <s27051@pjwstk.edu.pl>
2024-03-16 16:17:58 +0100

    Modification 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+//Modification 1
 Console.WriteLine("Mod 1");
+
+//Modification 2
+int myNum = 5;
+double myDoubleNum = 5.99D;
+char myLetter = 'D';
+bool myBool = true;
+string myText = "Hello";
